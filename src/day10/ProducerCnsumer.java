4d52232979ad45cdc31860225ad35af76fc5f91e
amanjeetkumar22 <amanjeetkumar31@gmail.com>
2026-02-28 00:00:26 +0530

    push

---
+++ src/day10/ProducerCnsumer.java
@@ -0,0 +1,37 @@
+package day10;
+class ProducerConsumer {
+
+    private static final int CAPACITY = 5;
+    private final java.util.Queue<Integer> queue = new java.util.LinkedList<>();
+
+    public void produce() throws InterruptedException {
+        int value = 0;
+        while (true) {
+            synchronized (this) {
+                while (queue.size() == CAPACITY) {
+                    wait();
+                }
+
+                System.out.println("Produced: " + value);
+                queue.add(value++);
+                notify();
+                Thread.sleep(500);
+            }
+        }
+    }
+
+    public void consume() throws InterruptedException {
+        while (true) {
+            synchronized (this) {
+                while (queue.isEmpty()) {
+                    wait();
+                }
+
+                int val = queue.poll();
+                System.out.println("Consumed: " + val);
+                notify();
+                Thread.sleep(500);
+            }
+        }
+    }
+}

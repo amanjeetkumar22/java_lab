c1c4500765dab42a4ffb5567de5dce3ba6a830a3
amanjeetkumar22 <amanjeetkumar31@gmail.com>
2026-03-29 22:23:03 +0530

    final code

---
+++ src/classwork/ExceptionDemo.java
@@ -0,0 +1,22 @@
+package classwork;
+public class ExceptionDemo {
+
+    public static void main(String[] args) {
+
+        // a) ArrayIndexOutOfBoundsException
+        try {
+            int arr[] = {1, 2, 3};
+            System.out.println(arr[5]);   // invalid index
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException handled: " + e);
+        }
+
+        // b) NullPointerException
+        try {
+            String str = null;
+            System.out.println(str.length());   // accessing null object
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException handled: " + e);
+        }
+    }
+}
